4d19a133140571599ff0d9c9d05e31186d74c7b4
Igor Lins e Silva <igor.linsesilva@gmail.com>
2021-09-16 04:17:59 -0300

    complete migration

--- hyperion-explorer/src/environments/environment.ts
+++ hyperion-explorer/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  eosioNodeUrl: 'http://192.168.0.20:8888',
-  hyperionApiUrl: 'http://192.168.0.20:7000',
+  eosioNodeUrl: 'http://192.168.10.120:7000',
+  hyperionApiUrl: 'http://192.168.10.120:7000',
   systemContract: 'eosio',
   userResourcesTable: 'userres'
 };
