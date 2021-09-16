f72a3cfa9f4bbfb58c332a810f6671c61217355a
Igor Lins e Silva <igor.linsesilva@gmail.com>
2021-09-16 04:10:26 -0300

    update cli

--- hyperion-explorer/src/test.js
+++ hyperion-explorer/src/test.js
@@ -1,7 +1,7 @@
 "use strict";
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 Object.defineProperty(exports, "__esModule", { value: true });
-require("zone.js/dist/zone-testing");
+require("zone.js/testing");
 const testing_1 = require("@angular/core/testing");
 const testing_2 = require("@angular/platform-browser-dynamic/testing");
 // First, initialize the Angular testing environment.
